d8a8bf190268df8b138941a189839aaee3da7000
TimL94 <99834249+TimL94@users.noreply.github.com>
2024-05-23 19:53:35 -0400

    initial code upload

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const db =require('./config/connection')
+const routes = require('./routes')
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(routes);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')))
+}
+
+db.once('open', () =>{
+    app.listen(PORT, () => console.log(`now listening on localhose: ${PORT}`))
+})
