ccdf950c7c8016066bc7a36c43d08b41b22bf04a
TimL94 <99834249+TimL94@users.noreply.github.com>
2024-05-29 16:50:07 -0400

    added mutations for all models

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -12,6 +12,9 @@ const resolvers = {
         },
         jobs: async () => {
             return await Job.find({})
+        },
+        job: async (parent, args) => {
+            return await Job.findById(args.id).populate('employer').populate('worker');
         }
     },
 
@@ -37,6 +40,76 @@ const resolvers = {
             const worker = await Worker.findById(job.worker);
             return worker;
         }
+    },
+
+    Mutation: {
+
+        // This block handles creation, updating, and deletion of Jobs
+        addJob: async (parent, { name, description, employer }) => {
+            return await Job.create({ name, description, employer });
+        },
+        updateJob: async (parent, { id, worker, description, name }) => {
+            
+            // creates an update fields object and stores any selected arguments within
+            const updateFields = {};
+            if(worker) updateFields.worker = worker;
+            if(description) updateFields.description = description;
+            if(name) updateFields.name = name;
+            
+            return await Job.findByIdAndUpdate(
+                { _id: id },
+                updateFields,
+                { new: true }
+
+            );
+        },
+        deleteJob: async (parent, { id }) => {
+            return await Job.findByIdAndDelete({_id: id});
+        },
+
+        // This block handles creation, updating, and deletion of Workers
+        addWorker: async (parent, { username, email, password, profession }) => {
+            return await Worker.create({ username, email, password, profession });
+        },
+        updateWorker: async (parent, { id, username, email, password, profession }) => {
+            
+            // creates an update fields object and stores any selected arguments within
+            const updateFields = {};
+            if(username) updateFields.username = username;
+            if(email) updateFields.email = email;
+            if(password) updateFields.password = password;
+            if(profession) updateFields.profession = profession;
+            
+            return await Worker.findOneAndUpdate(
+                { _id: id },
+                updateFields,
+                { new: true }
+            )
+        },
+        deleteWorker: async (parent, { id }) => {
+            return await Worker.findByIdAndDelete({ _id: id })
+        },
+
+        // This block handles creation, updating, and deletion of Employers
+        addEmployer: async (parent, { username, email, password }) => {
+            return await Employer.create({username, email, password});
+        },
+        updateEmployer: async (parent, {id, username, email, password}) => {
+            const updateFields = {};
+            if(username) updateFields.username = username;
+            if(email) updateFields.email = email;
+            if(password) updateFields.password = password;
+
+            return await Employer.findByIdAndUpdate(
+                { _id: id },
+                updateFields,
+                { new: true }
+            );
+        } ,
+        deleteEmployer: async (parent, { id }) => {
+            return await Employer.findByIdAndDelete({ _id: id });
+        }   
+    
     }
 }
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -26,6 +26,21 @@ type Query {
     workers: [Worker]
     employers: [Employer]
     jobs: [Job]
+    job(id: ID!): Job
+}
+
+type Mutation {
+    addJob(name: String!, description: String!, employer: ID!): Job
+    updateJob(id: ID!, worker: ID, description: String, name: String): Job
+    deleteJob(id: ID!): Job
+
+    addWorker(username: String!, email: String!, password: String! profession: String!): Worker
+    updateWorker(id: ID!, username: String, email: String, password: String, profession: String): Worker
+    deleteWorker(id: ID!): Worker
+
+    addEmployer(username: String!, email: String!, password: String!): Employer
+    updateEmployer(id: ID!, username: String, email: String, password: String): Employer
+    deleteEmployer(id: ID!): Employer
 }
 `
 module.exports = typeDefs;
