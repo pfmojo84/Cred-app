9f7c9fa0c382ccc770ce8e49ec35eccead6a4a25
jdporr <jdporrazzo@gmail.com>
2024-05-30 20:12:19 -0400

    add dropdown component and basic select to signin page

---
+++ client/src/components/UserDropDown.jsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
+export default function BasicSelect() {
+  const [age, setAge] = React.useState('');
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
+  return (
+    <Box sx={{ minWidth: 120 }} marginTop={2}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">User Type</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={age}
+          label="Age"
+          onChange={handleChange}
+        >
+          <MenuItem value={'Worker'}>Worker</MenuItem>
+          <MenuItem value={'Employer'}>Employer</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
+  );
+}
