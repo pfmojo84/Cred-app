dba211e904992903b9227b3afb208f247b81d9f8
jdporr <jdporrazzo@gmail.com>
2024-06-10 13:42:16 -0400

    update mongoose connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,6 +3,6 @@ require('dotenv').config();
 
 const databaseUrl = process.env.DB_URL;
 
-mongoose.connect(process.env.MONGODB_URI || databaseUrl);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/jobcred');
 
 module.exports = mongoose.connection;
